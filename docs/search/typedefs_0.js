035318436d1f11e8ed1b72ffe987afc28e143b91
Tim Schwartz <tim@metaverse.systems>
2019-03-10 19:14:04 -0500

    Removed ConfigMap

--- docs/search/typedefs_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['configmap',['ConfigMap',['../ecs_8hpp.html#a7be25bfffcc782eea58e87aece5bdb50',1,'ecs.hpp']]]
-];
--- include/libecs-cpp/ecs.hpp
+++ include/libecs-cpp/ecs.hpp
@@ -7,8 +7,4 @@
 #include <libecs-cpp/Component.hpp>
 #include <libecs-cpp/Entity.hpp>
 
-#include <string>
-#include <map>
-typedef std::map<std::string, std::string> ConfigMap;
-
 extern ecs::Manager *ECS;
