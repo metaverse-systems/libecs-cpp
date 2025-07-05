d069941d2a2f0ed93387af66d4089abcd2a0d2f1
Tim Schwartz <tim@metaverse.systems>
2025-07-05 07:12:50 -0500

    feat: implement Uuid class with conditional UUID generation and parsing

--- include/libecs-cpp/Uuid.hpp
+++ include/libecs-cpp/Uuid.hpp
@@ -8,19 +8,9 @@
       class Uuid
       {
         public:
-          Uuid()
-          {
-              UUIDv4::UUIDGenerator<std::mt19937_64> uuidGenerator;
-              this->id = uuidGenerator.getUUID();
-          }
-          Uuid(std::string id)
-          {
-              this->id = UUIDv4::UUID(id);
-          }
-          std::string Get()
-          {
-              return this->id.str();
-          }
+          Uuid();
+          Uuid(std::string id);
+          std::string Get();
         private:
           UUIDv4::UUID id;
       };
@@ -32,20 +22,9 @@
       class Uuid
       {
         public:
-          Uuid()
-          {
-              uuid_generate(this->id);
-          }
-          Uuid(std::string id)
-          {
-              uuid_parse(id.c_str(), this->id);
-          }
-          std::string Get()
-          {
-              char str_id[37] = {0};
-              uuid_unparse(this->id, str_id);
-              return std::string(str_id);
-          }
+          Uuid();
+          Uuid(std::string id);
+          std::string Get();
         private:
           uuid_t id;
       };
---
+++ src/Uuid.cpp
@@ -0,0 +1,36 @@
+#include <libecs-cpp/ecs.hpp>
+
+#ifndef DISABLE_BUILTIN_UUID
+ecs::Uuid::Uuid()
+{
+    UUIDv4::UUIDGenerator<std::mt19937_64> uuidGenerator;
+    this->id = uuidGenerator.getUUID();
+}
+
+ecs::Uuid::Uuid(std::string id)
+{
+    this->id = UUIDv4::UUID(id);
+}
+
+std::string ecs::Uuid::Get()
+{
+    return this->id.str();
+}
+#else
+ecs::Uuid::Uuid()
+{
+    uuid_generate(this->id);
+}
+
+ecs::Uuid::Uuid(std::string id)
+{
+    uuid_parse(id.c_str(), this->id);
+}
+
+std::string ecs::Uuid::Get()
+{
+    char str_id[37] = {0};
+    uuid_unparse(this->id, str_id); // 36 characters + 1 for null terminator
+    return std::string(str_id);
+}
+#endif
